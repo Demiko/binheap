1f4645fd8127ac4f5e1f75a833c4841614477c82
bronya2057 <bronya10@gmail.com>
2016-10-13 14:39:19 +0300

    bronya2057 ver of heapsort C

---
+++ bronya2057/heapsort.c
@@ -0,0 +1,66 @@
+
+#include <stdio.h>
+#define NMAX 20
+int arr[NMAX + 1];
+void swap(int*, int*);
+void heapify(int pos, int n) {
+	while (2 * pos + 1 < n) {
+
+		int t = 2 * pos + 1;
+		if (2 * pos + 2 < n && arr[2 * pos + 2] >= arr[t]) 
+		{
+			t = 2 * pos + 2;
+		}
+		if (arr[pos] < arr[t]) {
+			swap(&arr[pos], &arr[t]);
+			pos = t;
+		}
+		else break;
+
+	}
+}
+
+void heap_make(int n)
+{
+	for (int i = n - 1; i >= 0; i--)
+	{
+		heapify(i, n);
+	}
+}
+void heap_sort(int n)
+{
+	heap_make(n);
+	while (n>0)
+	{
+		swap(&arr[0], &arr[n - 1]);
+		n--;
+		heapify(0, n);
+	}
+}
+
+void swap(int* a, int*b )
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int main()
+{
+
+	int n;
+	printf("enter the number of elements\n");
+	scanf("%d",&n);
+	printf("Enter each element\n");
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	heap_sort(n);
+    printf("----------\n");
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d\n", arr[i]);;
+	}
+	return 0;
+}
